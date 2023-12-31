1919fae0c9b0ebde5d7f963cb2000fe14c70f281
Reed Dunkle <11543198+reeddunkle@users.noreply.github.com>
2023-12-31 00:49:52 -0500

    add ids to statuseffects

--- data/format.js
+++ data/format.js
@@ -2,13 +2,45 @@ const fs = require("node:fs");
 
 const fsp = fs.promises;
 
-const filePath = "./skills.json";
+// Dependencies
+
+const dependentPath = "./statusEffects.json";
+const depFile = fs.readFileSync(dependentPath);
+
+const _statusEffects = JSON.parse(depFile);
+
+function findStatusEffectByName(name) {
+  const found = _statusEffects.find(
+    (se) => se.name.toLowerCase() === name.toLowerCase(),
+  );
+
+  if (!found) {
+    throw Error(`'${name}' not found!!!`);
+  }
+
+  return found;
+}
+
+// DATA MANIPULATING
+const filePath = "./skills.json"; // <----- EDITING THIS
 
 const file = fs.readFileSync(filePath);
 
-const effects = JSON.parse(file);
+// Formatting
 
-const newJson = JSON.stringify(Object.values(effects), null, 2);
+const skills = JSON.parse(file);
+
+const RESULT = skills.map((obj) => ({
+  ...obj,
+  statusEffects: obj.statusEffects.map((se) => ({
+    ...se,
+    id: findStatusEffectByName(se.name).id,
+  })),
+}));
+
+// Writing to file
+
+const newJson = JSON.stringify(RESULT, null, 2);
 
 fsp
   .writeFile(filePath, newJson)
