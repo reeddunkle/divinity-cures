63b713016564f76e5b893038e483c0d65111cd60
Reed Dunkle <11543198+reeddunkle@users.noreply.github.com>
2024-01-01 18:55:52 -0500

    Increase size of points

--- components/point-costs.css.ts
+++ components/point-costs.css.ts
@@ -6,8 +6,8 @@ import scale from "@/util/scale";
 export const point = style({
   borderRadius: "50%",
   boxShadow: `0 0 15px .01px ${theme.color.foregroundDark}`,
-  height: scale.fluid.half.min.md,
-  width: scale.fluid.half.min.md,
+  height: scale.fluid.half.max.md,
+  width: scale.fluid.half.max.md,
 });
 
 export const pointCosts = style({
