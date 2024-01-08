9affef2fc630f53f247f602d93e3df3f81c367bc
Reed Dunkle <11543198+reeddunkle@users.noreply.github.com>
2024-01-08 13:24:37 -0500

    hover effect

--- components/cure-badge.css.ts
+++ components/cure-badge.css.ts
@@ -8,22 +8,25 @@ export const badgeLink = style({
   backgroundColor: theme.color.violet[1200],
   border: "1px solid transparent", // Hack: Occupy space
   borderRadius: "3px 3px 3px 3px",
-  boxShadow: `1px 1px 5px 0px ${hsl(0, 4, 30)}`,
+  boxShadow: "0px 2px 7px 0px rgba(255, 255, 255, 0.3)",
   color: theme.color.yellow[100],
   fontWeight: 600,
   lineHeight: 1,
   padding: scale.fluid.half.min.xxs,
   textAlign: "center",
   textTransform: "capitalize",
+  transition: "all ease 0.2s",
 
   ":hover": {
     backgroundColor: theme.color.violet[1100],
     border: "1px solid hsl(285 40% 40%)",
+    boxShadow: "0px 0px 10px 2px rgba(255, 255, 255, 0.25)",
+    // translate: "0 -2px",
   },
 
   ":active": {
     backgroundColor: theme.color.violet[1000],
-    boxShadow: "none",
+    boxShadow: `inset 0px 0px 5px 1px ${hsl(0, 4, 30)}`,
   },
 });
 
