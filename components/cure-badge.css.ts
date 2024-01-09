85957870a1b7ce61417be395e7a2d8359ac06082
Reed Dunkle <11543198+reeddunkle@users.noreply.github.com>
2024-01-08 22:57:19 -0500

    styles

--- components/cure-badge.css.ts
+++ components/cure-badge.css.ts
@@ -8,7 +8,7 @@ export const badgeLink = style({
   backgroundColor: theme.color.violet[1200],
   border: "1px solid transparent", // Hack: Occupy space
   borderRadius: "3px 3px 3px 3px",
-  boxShadow: "0px 2px 7px 0px rgba(255, 255, 255, 0.3)",
+  boxShadow: "2px 1px 7px 1px rgba(255, 255, 255, 0.3)",
   color: theme.color.yellow[100],
   fontWeight: 600,
   lineHeight: 1,
@@ -20,13 +20,12 @@ export const badgeLink = style({
   ":hover": {
     backgroundColor: theme.color.violet[1100],
     border: "1px solid hsl(285 40% 40%)",
-    boxShadow: "0px 0px 10px 2px rgba(255, 255, 255, 0.25)",
-    // translate: "0 -2px",
+    boxShadow: "1px 0px 10px 2px rgba(255, 255, 255, 0.25)",
   },
 
   ":active": {
     backgroundColor: theme.color.violet[1000],
-    boxShadow: `inset 0px 0px 5px 1px ${hsl(0, 4, 30)}`,
+    boxShadow: "inset 0px 0px 5px 1px rgba(255, 255, 255, 0.25)",
   },
 });
 
